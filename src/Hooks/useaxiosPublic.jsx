edb619cbcf27d8c3f6b38fbb3d0ae9cf57d28e83
Mohammed Rifatul Islam <mdrifatulislam08@gmail.com>
2024-07-04 11:12:32 +0600

    url change

--- src/Hooks/useaxiosPublic.jsx
+++ src/Hooks/useaxiosPublic.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 
 const axiosPublic = axios.create({
   // baseURL: 'https://challengeforge-server.vercel.app'
-  baseURL: 'http://localhost:5000'
+  baseURL: 'https://challengeforge-server.vercel.app'
 })
 
 const useaxiosPublic = () => {
